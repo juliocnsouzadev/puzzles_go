9d347765d6a61d95c930eb17f26bf316c848494d
juliocnsouzadev <juliocnsouzadev@gmail.com>
2022-12-19 00:08:38 +0000

    feat: find closest in bst

---
+++ bst/bst.go
@@ -0,0 +1,50 @@
+package bst
+
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+func (tree *BST) FindClosestValue(target int) int {
+	return tree.findClosestValue(target, -1, nil)
+}
+
+func (tree *BST) findClosestValue(target int, lowestDiff int, holder *BST) int {
+	if tree.Value == target {
+		return tree.Value
+	}
+
+	if holder == nil {
+		lowestDiff = tree.diff(target)
+		holder = tree
+	}
+
+	if tree.diff(target) < lowestDiff {
+		lowestDiff = tree.diff(target)
+		holder = tree
+	}
+
+	if tree.Left != nil && target < tree.Value {
+		return tree.Left.findClosestValue(target, lowestDiff, holder)
+	}
+
+	if tree.Right != nil && target > tree.Value {
+		return tree.Right.findClosestValue(target, lowestDiff, holder)
+	}
+
+	return holder.Value
+}
+
+func (tree *BST) diff(target int) int {
+	diff := tree.Value - target
+	if diff < 0 {
+		diff = diff * -1
+	}
+	return diff
+}
+
+func (tree *BST) isLeaf() bool {
+	return tree.Left == nil && tree.Right == nil
+}

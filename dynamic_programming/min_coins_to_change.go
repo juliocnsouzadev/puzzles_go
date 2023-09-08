faeaf29a8ae88bf34da9a38c122fd8d0b9f012f1
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-09-08 13:20:20 +0100

    refactor: min coins

--- dynamic_programming/min_coins_to_change.go
+++ dynamic_programming/min_coins_to_change.go
@@ -1,8 +1,11 @@
 package dynamic_programming
 
+import "sort"
+
 func MinCoinsToChange(amount int, coins []int) int {
-	totalOfCoins := 0
-	amountLeft := amount //initially, the amount left is the amount itself
+	sort.Ints(coins) //sort the coins
+
+	minCoins := -1 //initially, we don't have a solution
 
 	for i := len(coins) - 1; i >= 0; i-- { //iterate from the biggest coin to the smallest
 		if coins[i] > amount { //if the coin is bigger than the amount, skip it
@@ -11,13 +14,19 @@ func MinCoinsToChange(amount int, coins []int) int {
 		if coins[i] == amount { //jackpot!
 			return 1
 		}
-		var nCoins = amountLeft / coins[i] //find the current number of coins
-		amountLeft = amountLeft % coins[i] //find the amount left
-		totalOfCoins += nCoins             //add the number of coins to the total
 
-		if amountLeft == 0 { //if there is no amount left, we found a solution
-			return totalOfCoins
+		totalOfCoins := 0
+		amountLeft := amount //initially, the amount left is the amount itself
+
+		for j := i; j >= 0; j-- {
+			var nCoins = amountLeft / coins[j] //find the current number of coins
+			amountLeft = amountLeft % coins[j] //find the amount left
+			totalOfCoins += nCoins             //add the number of coins to the total
+		}
+
+		if amountLeft == 0 && minCoins != -1 && totalOfCoins < minCoins { //if there is no amount left, we found a solution
+			totalOfCoins = minCoins
 		}
 	}
-	return -1 //if we got here, it means that we couldn't find a solution
+	return minCoins
 }
